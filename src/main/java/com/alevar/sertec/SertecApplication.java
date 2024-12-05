203bd081e1bf5d55b2666a954102eee643893663
Alejandro84 <thejanoo84@hotmail.com>
2024-12-05 11:24:13 -0300

    cargado de archivos springboot

---
+++ src/main/java/com/alevar/sertec/SertecApplication.java
@@ -0,0 +1,13 @@
+package com.alevar.sertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SertecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SertecApplication.class, args);
+	}
+
+}
